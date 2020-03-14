efd06e3af516af3c3744651cd822f702d298fd23
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-03-14 14:18:14 +0100

    init

---
+++ src/main/java/no/nav/permitteringsskjemaapi/PermitteringsskjemaApiApplication.java
@@ -0,0 +1,13 @@
+package no.nav.permitteringsskjemaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermitteringsskjemaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PermitteringsskjemaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/nav/permitteringsskjemaapi/PermitteringsskjemaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package no.nav.permitteringsskjemaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermitteringsskjemaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
