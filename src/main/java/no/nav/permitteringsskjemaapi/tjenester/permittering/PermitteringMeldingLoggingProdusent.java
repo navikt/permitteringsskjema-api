c62136f74bfc5f0a7ee5ab86f57b43ed17d8686b
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-03-15 20:08:13 +0100

    logging is disabled

--- src/main/java/no/nav/permitteringsskjemaapi/tjenester/permittering/PermitteringMeldingLoggingProdusent.java
+++ src/main/java/no/nav/permitteringsskjemaapi/tjenester/permittering/PermitteringMeldingLoggingProdusent.java
@@ -2,13 +2,13 @@ package no.nav.permitteringsskjemaapi.tjenester.permittering;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Service;
 
 import no.nav.permitteringsskjemaapi.Permitteringsskjema;
 
 @Service
-@ConditionalOnProperty(name = "permittering.enabled", havingValue = "false")
+@ConditionalOnMissingBean(PermitteringMeldingKafkaProdusent.class)
 public class PermitteringMeldingLoggingProdusent implements Permittering {
 
     private static final Logger LOG = LoggerFactory.getLogger(PermitteringMeldingLoggingProdusent.class);
