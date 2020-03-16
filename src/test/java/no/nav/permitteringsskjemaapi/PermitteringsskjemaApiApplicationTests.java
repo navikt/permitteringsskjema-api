d842ff56dcf86cb10e8eba23c3a8f0e788287664
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-03-16 12:28:41 +0100

    disable vault i test

--- src/test/java/no/nav/permitteringsskjemaapi/PermitteringsskjemaApiApplicationTests.java
+++ src/test/java/no/nav/permitteringsskjemaapi/PermitteringsskjemaApiApplicationTests.java
@@ -2,8 +2,11 @@ package no.nav.permitteringsskjemaapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = { "spring.cloud.vault.enabled=false" })
+
 class PermitteringsskjemaApiApplicationTests {
 
     @Test
