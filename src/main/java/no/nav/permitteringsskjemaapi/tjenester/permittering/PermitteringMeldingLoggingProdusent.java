471ad22062b777518babf439f2088e2c87c82a86
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-03-17 14:11:01 +0100

    lytt på event

--- src/main/java/no/nav/permitteringsskjemaapi/tjenester/permittering/PermitteringMeldingLoggingProdusent.java
+++ src/main/java/no/nav/permitteringsskjemaapi/tjenester/permittering/PermitteringMeldingLoggingProdusent.java
@@ -3,15 +3,25 @@ package no.nav.permitteringsskjemaapi.tjenester.permittering;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import no.nav.permitteringsskjemaapi.PermittertPerson;
+import no.nav.permitteringsskjemaapi.domenehendelser.SkjemaSendtInn;
 
 @Service
 @ConditionalOnMissingBean(PermitteringMeldingKafkaProdusent.class)
 public class PermitteringMeldingLoggingProdusent implements Permittering {
 
     private static final Logger LOG = LoggerFactory.getLogger(PermitteringMeldingLoggingProdusent.class);
+
+    @EventListener
+    public void sendInn(SkjemaSendtInn event) {
+        LOG.info("Skjema sendt inn id={}", event.getPermitteringsskjema().getId());
+        event.getPermitteringsskjema()
+                .permittertePersoner().stream()
+                .forEach(this::publiser);
+    }
 
     @Override
     public void publiser(PermittertPerson person) {
