9aafdbe58f6effc1439690ef1c5a2b25550d7929
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-03-18 10:26:55 +0100

    fiks slik at det kjører lokalt

--- src/main/java/no/nav/permitteringsskjemaapi/altinn/OrganisasjonerController.java
+++ src/main/java/no/nav/permitteringsskjemaapi/altinn/OrganisasjonerController.java
@@ -3,6 +3,7 @@ package no.nav.permitteringsskjemaapi.altinn;
 import lombok.AllArgsConstructor;
 import no.nav.permitteringsskjemaapi.util.FnrExtractor;
 import no.nav.security.token.support.core.api.Protected;
+import no.nav.security.token.support.core.api.Unprotected;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +14,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/organisasjoner")
 @AllArgsConstructor
-@Protected
+@Unprotected
 public class OrganisasjonerController {
 
     private final AltinnService altinnService;
     private FnrExtractor fnrExtractor;
-
     @GetMapping
+    public List<AltinnOrganisasjon> hent() {
+        return List.of(new AltinnOrganisasjon("STORFOSNA OG FREDRIKSTAD REGNSKAP", "Business", "910825569", "BEDR", "Active", "910825550"));
+    }
+
     public ResponseEntity<List<AltinnOrganisasjon>> hentOrganisasjoner() {
         String fnr = fnrExtractor.extract();
         List <AltinnOrganisasjon> result = altinnService.hentOrganisasjoner(fnr);
