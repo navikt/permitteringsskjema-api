86606fa060b46ba8b2dca2d613f1d4da474a8876
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-03-19 12:21:01 +0100

    lagt til bedre feilhåndtering

---
+++ src/main/java/no/nav/permitteringsskjemaapi/exceptions/ApiError.java
@@ -0,0 +1,78 @@
+package no.nav.permitteringsskjemaapi.exceptions;
+
+import static com.fasterxml.jackson.annotation.JsonFormat.Feature.WRITE_SINGLE_ELEM_ARRAYS_UNWRAPPED;
+import static com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING;
+import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
+import static no.nav.permitteringsskjemaapi.util.MDCUtil.callId;
+import static no.nav.permitteringsskjemaapi.util.StreamUtil.safeStream;
+import static org.springframework.core.NestedExceptionUtils.getMostSpecificCause;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.http.HttpStatus;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.google.common.collect.Lists;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiError {
+    private final HttpStatus status;
+    @JsonFormat(shape = STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private final LocalDateTime timestamp;
+    @JsonFormat(with = WRITE_SINGLE_ELEM_ARRAYS_UNWRAPPED)
+    private final List<String> messages;
+    private final String uuid;
+
+    public ApiError(HttpStatus status, Throwable t) {
+        this(status, t, emptyList());
+    }
+
+    public ApiError(HttpStatus status, Object... objects) {
+        this(status, null, Arrays.asList(objects));
+    }
+
+    ApiError(HttpStatus status, Throwable t, List<Object> objects) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status;
+        this.messages = messages(t, objects);
+        this.uuid = callId();
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public List<String> getMessages() {
+        return messages;
+    }
+
+    private static List<String> messages(Throwable t, List<Object> objects) {
+        List<Object> messages = Lists.newArrayList(objects);
+        if (t != null) {
+            messages.add(getMostSpecificCause(t).getMessage());
+        }
+        return safeStream(messages)
+                .filter(Objects::nonNull)
+                .map(Object::toString)
+                .collect(toList());
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "[status=" + status + ", timestamp=" + timestamp + ", messages=" + messages
+                + ", uuid=" + uuid + "]";
+    }
+}
---
+++ src/main/java/no/nav/permitteringsskjemaapi/exceptions/PermitteringExceptionHandler.java
@@ -0,0 +1,116 @@
+package no.nav.permitteringsskjemaapi.exceptions;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import no.nav.permitteringsskjemaapi.util.TokenUtil;
+import no.nav.security.token.support.core.exceptions.JwtTokenValidatorException;
+import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException;
+
+@ControllerAdvice
+public class PermitteringExceptionHandler extends ResponseEntityExceptionHandler {
+    @Inject
+    private TokenUtil tokenUtil;
+    private static final Logger LOG = LoggerFactory.getLogger(PermitteringExceptionHandler.class);
+
+    @ResponseBody
+    @ExceptionHandler(HttpStatusCodeException.class)
+    public ResponseEntity<Object> handleHttpStatusCodeException(HttpStatusCodeException e, WebRequest request) {
+        if (e.getStatusCode().equals(UNAUTHORIZED) || e.getStatusCode().equals(FORBIDDEN)) {
+            return logAndHandle(e.getStatusCode(), e, request, tokenUtil.getExpiryDate());
+        }
+        return logAndHandle(e.getStatusCode(), e, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e,
+            HttpHeaders headers, HttpStatus status, WebRequest req) {
+        return logAndHandle(UNPROCESSABLE_ENTITY, e, req, headers, validationErrors(e));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException e,
+            HttpHeaders headers, HttpStatus status, WebRequest req) {
+        return logAndHandle(UNPROCESSABLE_ENTITY, e, req, headers);
+    }
+
+    @ExceptionHandler({ UnexpectedInputException.class })
+    public ResponseEntity<Object> handleUnexpectedException(UnexpectedInputException e, WebRequest req) {
+        return logAndHandle(UNPROCESSABLE_ENTITY, e, req);
+    }
+
+    @ExceptionHandler({ JwtTokenUnauthorizedException.class })
+    public ResponseEntity<Object> handleUnauthorizedException(JwtTokenUnauthorizedException e, WebRequest req) {
+        return logAndHandle(UNAUTHORIZED, e, req);
+    }
+
+    @ExceptionHandler({ JwtTokenValidatorException.class })
+    public ResponseEntity<Object> handleUnauthenticatedJwtException(JwtTokenValidatorException e, WebRequest req) {
+        return logAndHandle(FORBIDDEN, e, req, e.getExpiryDate());
+    }
+
+    @ExceptionHandler(value = { Exception.class })
+    protected ResponseEntity<Object> handleUncaught(Exception e, WebRequest req) {
+        return logAndHandle(INTERNAL_SERVER_ERROR, e, req);
+    }
+
+    private ResponseEntity<Object> logAndHandle(HttpStatus status, Exception e, WebRequest req, Object... messages) {
+        return logAndHandle(status, e, req, new HttpHeaders(), messages);
+    }
+
+    private ResponseEntity<Object> logAndHandle(HttpStatus status, Exception e, WebRequest req, HttpHeaders headers,
+            Object... messages) {
+        return logAndHandle(status, e, req, headers, asList(messages));
+    }
+
+    private ResponseEntity<Object> logAndHandle(HttpStatus status, Exception e, WebRequest req, HttpHeaders headers,
+            List<Object> messages) {
+        ApiError apiError = apiErrorFra(status, e, messages);
+        LOG.warn("({}) {} {} ({})", subject(), status, apiError.getMessages(), status.value(), e);
+        return handleExceptionInternal(e, apiError, headers, status, req);
+    }
+
+    private String subject() {
+        return Optional.ofNullable(tokenUtil.getSubject())
+                .orElse("Uautentisert");
+    }
+
+    private static ApiError apiErrorFra(HttpStatus status, Exception e, List<Object> messages) {
+        return new ApiError(status, e, messages);
+    }
+
+    private static List<String> validationErrors(MethodArgumentNotValidException e) {
+        return e.getBindingResult().getFieldErrors()
+                .stream()
+                .map(PermitteringExceptionHandler::errorMessage)
+                .collect(toList());
+    }
+
+    private static String errorMessage(FieldError error) {
+        return error.getField() + " " + error.getDefaultMessage();
+    }
+}
