99a71928bc4e6b496e21e3ac2b64532b78213c8c
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-03-19 11:01:58 +0100

    flere bedrifter lokal

--- src/main/java/no/nav/permitteringsskjemaapi/altinn/FakeAltinnService.java
+++ src/main/java/no/nav/permitteringsskjemaapi/altinn/FakeAltinnService.java
@@ -12,6 +12,10 @@ import java.util.List;
 public class FakeAltinnService implements AltinnService {
     @Override
     public List<AltinnOrganisasjon> hentOrganisasjoner(String fnr) {
-        return List.of(new AltinnOrganisasjon("STORFOSNA OG FREDRIKSTAD REGNSKAP", "Business", "910825569", "BEDR", "Active", "910825550"),new AltinnOrganisasjon("BALLSTAD OG HORTEN", "Enterprise", "910825550", "AS", "Active", null));
+        return List.of(new AltinnOrganisasjon("STORFOSNA OG FREDRIKSTAD REGNSKAP", "Business", "910825569", "BEDR", "Active", "910825550"),
+                new AltinnOrganisasjon("BALLSTAD OG HORTEN", "Enterprise", "910825550", "AS", "Active", null),
+                new AltinnOrganisasjon("BAREKSTAD OG YTTERVÅG REGNSKAP", "Enterprise", "910998250", "AS", "Active", null),
+                new AltinnOrganisasjon("BALLSTAD OG HORTEN", "Business", "810514442", "BEDR", "Active", "910998250"));
+
     }
 }
