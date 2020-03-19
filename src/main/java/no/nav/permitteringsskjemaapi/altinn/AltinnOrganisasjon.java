1929a9a8450a6868d301686e3c58d7397d804208
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-03-19 08:28:19 +0100

    forventer stor forbokstav i feltnavn fra altinn

--- src/main/java/no/nav/permitteringsskjemaapi/altinn/AltinnOrganisasjon.java
+++ src/main/java/no/nav/permitteringsskjemaapi/altinn/AltinnOrganisasjon.java
@@ -4,10 +4,16 @@ import lombok.Value;
 
 @Value
 public class AltinnOrganisasjon {
+    @JsonProperty("Name")
     String Name;
+    @JsonProperty("Type")
     String Type;
+    @JsonProperty("OrganizationNumber")
     String OrganizationNumber;
+    @JsonProperty("OrganizationForm")
     String OrganizationForm;
+    @JsonProperty("Status")
     String Status;
+    @JsonProperty("ParentOrganizationNumber")
     String ParentOrganizationNumber;
 }
