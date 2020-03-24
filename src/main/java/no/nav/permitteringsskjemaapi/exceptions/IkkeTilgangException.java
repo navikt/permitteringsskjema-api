3b3294b6f677cb6b453c356c3a0f2860f454d5f9
Mats Bolstad <mbolstad@gmail.com>
2020-03-24 10:22:47 +0100

    Retter feil ved merge

--- src/main/java/no/nav/permitteringsskjemaapi/exceptions/IkkeTilgangException.java
+++ src/main/java/no/nav/permitteringsskjemaapi/exceptions/IkkeTilgangException.java
@@ -1,8 +1,4 @@
 package no.nav.permitteringsskjemaapi.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.FORBIDDEN)
 public class IkkeTilgangException extends RuntimeException {
 }
