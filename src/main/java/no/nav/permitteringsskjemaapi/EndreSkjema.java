a346aa276d715e1cbfebae5291d3e3404ba4def5
Mats Bolstad <mbolstad@gmail.com>
2020-03-27 11:57:46 +0100

    Yrkeskategorier som nytt felt (#100)
    * Yrkeskategorier som nytt felt
    
    * Tar med antall per yrkeskategori også

--- src/main/java/no/nav/permitteringsskjemaapi/EndreSkjema.java
+++ src/main/java/no/nav/permitteringsskjemaapi/EndreSkjema.java
@@ -23,6 +23,7 @@ public class EndreSkjema {
     String fritekst;
     Integer antallBerørt;
     List<Person> personer = new ArrayList<>();
+    List<Yrkeskategori> yrkeskategorier = new ArrayList<>();
     Årsakskode årsakskode;
     String årsakstekst;
 }
