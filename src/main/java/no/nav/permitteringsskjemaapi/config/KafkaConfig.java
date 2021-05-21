f6c21664654d458c45650b6c57a3dd2c9a1ca078
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2021-05-21 16:23:25 +0200

    lagt til serializer i kafka config

--- src/main/java/no/nav/permitteringsskjemaapi/config/KafkaConfig.java
+++ src/main/java/no/nav/permitteringsskjemaapi/config/KafkaConfig.java
@@ -1,6 +1,8 @@
 package no.nav.permitteringsskjemaapi.config;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.config.SslConfigs;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +18,6 @@ public class KafkaConfig {
     private final String keystorePath;
     private final String credstorePassword;
     private final String truststorePath;
-
 
     public KafkaConfig(
             @Value("${kafka.keystore.path}") String keystorePath,
@@ -39,6 +40,10 @@ public class KafkaConfig {
         configMap.put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG,"PKCS12");
         configMap.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG,truststorePath);
         configMap.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG,"SSL");
+        configMap.put(ProducerConfig.CLIENT_ID_CONFIG,"permitteringsskjema-api");
+        configMap.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configMap.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
         return new KafkaTemplate(new DefaultKafkaProducerFactory(configMap));
     }
 }
