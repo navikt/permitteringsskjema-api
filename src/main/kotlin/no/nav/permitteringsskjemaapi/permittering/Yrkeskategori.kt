03679fb1ae88e8cf0cf677e2aece190e429f17e5
Ken Gullaksen <ken.gullaksen@nav.no>
2023-04-21 09:28:39 +0200

    legg tilbake samme equals som var med lombok

--- src/main/kotlin/no/nav/permitteringsskjemaapi/permittering/Yrkeskategori.kt
+++ src/main/kotlin/no/nav/permitteringsskjemaapi/permittering/Yrkeskategori.kt
@@ -20,4 +20,19 @@ data class Yrkeskategori(
     var styrk08: String? = null,
     var label: String? = null,
     var antall: Int? = null,
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Yrkeskategori
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
+    }
+}
