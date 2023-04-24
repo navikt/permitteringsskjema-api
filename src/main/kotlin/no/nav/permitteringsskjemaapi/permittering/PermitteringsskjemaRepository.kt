c0683de43397a613910641579d4e4fac6c1387de
Ken Gullaksen <ken.gullaksen@nav.no>
2023-04-24 12:19:35 +0200

    sorter skjema på sendtInn så opprettet tidspunkt

--- src/main/kotlin/no/nav/permitteringsskjemaapi/permittering/PermitteringsskjemaRepository.kt
+++ src/main/kotlin/no/nav/permitteringsskjemaapi/permittering/PermitteringsskjemaRepository.kt
@@ -1,11 +1,16 @@
 package no.nav.permitteringsskjemaapi.permittering
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.util.*
 
 interface PermitteringsskjemaRepository : JpaRepository<Permitteringsskjema?, UUID> {
     fun findByIdAndOpprettetAv(id: UUID, opprettetAv: String): Optional<Permitteringsskjema>
     override fun findById(id: UUID): Optional<Permitteringsskjema?>
+    @Query("" +
+            "select s from Permitteringsskjema s " +
+            "   where s.opprettetAv = :opprettetAv " +
+            "   order by s.sendtInnTidspunkt desc nulls last, s.opprettetTidspunkt desc")
     fun findAllByOpprettetAv(opprettetAv: String): List<Permitteringsskjema>
     fun findAllByBedriftNr(bedriftNr: String): List<Permitteringsskjema>
 }
--- src/test/kotlin/no/nav/permitteringsskjemaapi/PermitteringsskjemaRepositoryTest.kt
+++ src/test/kotlin/no/nav/permitteringsskjemaapi/PermitteringsskjemaRepositoryTest.kt
@@ -1,36 +1,42 @@
 package no.nav.permitteringsskjemaapi
 
-import no.nav.permitteringsskjemaapi.permittering.Permitteringsskjema
 import no.nav.permitteringsskjemaapi.permittering.PermitteringsskjemaRepository
 import org.assertj.core.api.Assertions
+import org.flywaydb.core.Flyway
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
+import java.time.Instant
+import java.time.temporal.ChronoUnit.MINUTES
+
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
+@SpringBootTest(properties = ["spring.flyway.cleanDisabled=false"])
 @ActiveProfiles("test")
-@DirtiesContext
 class PermitteringsskjemaRepositoryTest {
-    private var lagretPermitteringsskjema: Permitteringsskjema? = null
-    private var permitteringsskjema: Permitteringsskjema? = null
+
 
     @Autowired
     lateinit var repository: PermitteringsskjemaRepository
 
+    @Autowired
+    lateinit var flyway: Flyway
+
     @Before
-    fun setUp() {
-        permitteringsskjema = PermitteringTestData.enPermitteringMedAltFyltUt()
-        lagretPermitteringsskjema = repository.save(permitteringsskjema!!)
+    fun clearDatabase() {
+        flyway.clean()
+        flyway.migrate()
     }
 
     @Test
     fun skal_kunne_lagre_alle_felter() {
+        val permitteringsskjema = PermitteringTestData.enPermitteringMedAltFyltUt()
+        val lagretPermitteringsskjema = repository.save(permitteringsskjema)
+
         Assertions.assertThat(lagretPermitteringsskjema)
             .usingRecursiveComparison() // Felt-for-felt sammenligning
             .isEqualTo(permitteringsskjema)
@@ -38,7 +44,47 @@ class PermitteringsskjemaRepositoryTest {
 
     @Test
     fun skal_kunne_hentes_med_id() {
-        val hentetPermittering = repository.findById(permitteringsskjema!!.id!!)
+        val permitteringsskjema = PermitteringTestData.enPermitteringMedAltFyltUt()
+        repository.save(permitteringsskjema)
+        val hentetPermittering = repository.findById(permitteringsskjema.id!!)
         Assertions.assertThat(hentetPermittering).hasValue(permitteringsskjema)
+    }
+
+    @Test
+    fun `skal hente skjema sortert på sendtinn eller opprettet tidspunkt`() {
+        val now = Instant.now()
+        PermitteringTestData.enPermitteringMedAltFyltUt().apply {
+            bedriftNavn = "sendt inn 1 minutt siden, opprettet 10 min siden"
+            sendtInnTidspunkt = now.minus(1, MINUTES)
+            opprettetTidspunkt = now.minus(10, MINUTES)
+            opprettetAv = "me"
+        }.also { repository.save(it) }
+        PermitteringTestData.enPermitteringMedAltFyltUt().apply {
+            bedriftNavn = "ikke sendt inn opprettet 5 min siden"
+            sendtInnTidspunkt = null
+            opprettetTidspunkt = now.minus(5, MINUTES)
+            opprettetAv = "me"
+        }.also { repository.save(it) }
+        PermitteringTestData.enPermitteringMedAltFyltUt().apply {
+            bedriftNavn = "ikke sendt inn opprettet 10 min siden"
+            sendtInnTidspunkt = null
+            opprettetTidspunkt = now.minus(10, MINUTES)
+            opprettetAv = "me"
+        }.also { repository.save(it) }
+        PermitteringTestData.enPermitteringMedAltFyltUt().apply {
+            bedriftNavn = "sendt inn 2 minutt siden, opprettet 5 min siden"
+            sendtInnTidspunkt = now.minus(2, MINUTES)
+            opprettetTidspunkt = now.minus(5, MINUTES)
+            opprettetAv = "me"
+        }.also { repository.save(it) }
+
+        Assertions.assertThat(
+            repository.findAllByOpprettetAv("me").map { it.bedriftNavn!! }
+        ).containsExactly(
+            "sendt inn 1 minutt siden, opprettet 10 min siden",
+            "sendt inn 2 minutt siden, opprettet 5 min siden",
+            "ikke sendt inn opprettet 5 min siden",
+            "ikke sendt inn opprettet 10 min siden",
+        )
     }
 }
